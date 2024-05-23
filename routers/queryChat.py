60f6cfc556c73481bdc001ed7c68111eefc1838a
yqq <youngqqcn@gmail.com>
2024-05-23 18:58:49 +0800

    增加查询聊天

--- routers/queryChat.py
+++ routers/queryChat.py
@@ -1,8 +1,10 @@
+import json
 from typing import List
 from fastapi import APIRouter
 from pydantic import BaseModel
 
 from models import ResponseModel
+from utils import create_redis_client, create_response
 
 
 router = APIRouter()
@@ -14,13 +16,13 @@ class Req(BaseModel):
 
 
 class Msg(BaseModel):
-    msg: str
-    id: str
-    user: str
+    msg_id: str
+    content: str
+    user_id: str
+    timestamp: int
 
 
 class Resp(BaseModel):
-
     user_id: str
     room_id: str
     msgs: List[Msg]
@@ -28,7 +30,20 @@ class Resp(BaseModel):
 
 @router.post("", response_model=ResponseModel)
 async def handler(req: Req):
+
+    rdc = create_redis_client()
+
+    # 查询消息记录
+    msgs = rdc.zrange("chatchannel:" + req.room_id, 0, -1, desc=True)
+    rsp_msgs = []
+    if msgs is not None:
+        for item in msgs:
+            msg = json.loads(item)
+            rsp_msgs.append(Msg(**msg))
+
     rsp = Resp(
-        user_id=req.user_id, room_id="xxxxx", msgs=[Msg(msg="hello", id="1", user="A")]
+        user_id=req.user_id,
+        room_id=req.room_id,
+        msgs=rsp_msgs,
     )
-    return rsp
+    return create_response(data=rsp)
