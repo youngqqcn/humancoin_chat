9d4a6a727f1fe2ea83b45b8591c2a1da859c476c
yqq <youngqqcn@gmail.com>
2024-05-23 19:22:07 +0800

    增加结束聊天接口

--- routers/finishChat.py
+++ routers/finishChat.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 from pydantic import BaseModel
 
 from models import ResponseModel
+from utils import create_redis_client, create_response
 
 router = APIRouter()
 
@@ -21,5 +22,27 @@ class Resp(BaseModel):
 
 @router.post("", response_model=ResponseModel)
 async def handler(req: Req):
-    rsp = Resp(user_id=req.user_id, room_id="xxxxx", is_win=True, rewards=999)
-    return rsp
+    rdc = create_redis_client()
+
+    users = rdc.get('chatroom:' + req.room_id)
+    if users is None:
+        #TODO
+        pass
+
+
+    user_ids = str(users).split('vs')
+    assert len(user_ids) == 2, "invalid room"
+
+    opponent_user_id = user_ids[0] if req.user_id != user_ids[0] else user_ids[1]
+
+    opponent_role = 'bot' if opponent_user_id.startswith('bot') else 'human'
+    rsp = Resp(user_id=req.user_id, room_id=req.room_id, is_win=False, rewards=0)
+    guess_role = 'human' if req.human else 'bot'
+    if guess_role == opponent_role:
+        rsp.is_win = True
+        rsp.rewards = 100
+
+
+    # TODO: 更新聊天室的状态为结束
+
+    return create_response(data=rsp)
