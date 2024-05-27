4c4214a8a566efd928baafa3ef16fcd5cb76d6f7
yqq <youngqqcn@gmail.com>
2024-05-27 12:23:28 +0800

    完善工具

--- tools/chat.py
+++ tools/chat.py
@@ -1,11 +1,59 @@
 import time
 import requests
 
-# read msg from console
-if __name__ == "__main__":
+def finish_chat(user_id: str, room_id: str):
+    judge = input("聊天结束,对方是人类吗? 请输入  yes 或者 no: ")
+    assert judge.strip() in ["yes", "no"]
+    finish_rsp = requests.post(
+        "http://192.168.110.207:8000/finishChat",
+        json={
+            "user_id": user_id,
+            "room_id": room_id,
+            "human": True if judge.strip() == "yes" else False,
+        },
+    )
+
+    if finish_rsp is not None:
+        if finish_rsp.json()["data"]["is_win"]:
+            print("恭喜!你赢了!")
+        else:
+            print("很遗憾!你输了!")
+    return
+
+
+def query_chat(user_id: str, room_id: str):
+    rsp = requests.post(
+        "http://192.168.110.207:8000/queryChat",
+        json={
+            "user_id": user_id,
+            "room_id": room_id,
+        },
+    )
+    if not rsp.ok:
+        print("error: {}".format(rsp.text))
+        exit(0)
+    return rsp.json()
+
+
+def send_chat(user_id: str, room_id: str, user_input: str):
+    rsp = requests.post(
+        "http://192.168.110.207:8000/sendChatMsg",
+        json={
+            "user_id": user_id,
+            "msg": user_input.strip(),
+            "room_id": room_id,
+        },
+    )
+    if rsp is None or rsp.status_code != 200:
+        print("error: {}".format(rsp.text))
+        exit(0)
+    pass
+
+
+def main():
 
     # 输入user_id
-    user_id = input("please input your userid:")
+    user_id = input("请输入用户id:")
 
     # 开始匹配
     rsp = requests.post(
@@ -18,26 +66,26 @@ if __name__ == "__main__":
     room_id = rsp.json()["data"]["room_id"]
     print("房间号: {}".format(room_id))
     while True:
+        waiting_msg = False
         while True:
-            rsp = requests.post(
-                "http://192.168.110.207:8000/queryChat",
-                json={
-                    "user_id": user_id,
-                    "room_id": room_id,
-                },
-            )
-            if not rsp.ok:
-                print("error: {}".format(rsp.text))
-                exit(0)
+            rsp = query_chat(user_id=user_id, room_id=room_id)
+            # 如果聊天已经结束
+            if rsp["data"]["is_time_up"]:
+                finish_chat(user_id=user_id, room_id=room_id)
+                return
 
-            if rsp.json()["data"]["is_my_turn"]:
-                if len(rsp.json()["data"]["msgs"]) > 0:
-                    latest_msg = rsp.json()["data"]["msgs"][0]['content']
-                    print("Other: {}".format(latest_msg))
+            if rsp["data"]["is_my_turn"]:
+                if len(rsp["data"]["msgs"]) > 0:
+                    latest_msg = rsp["data"]["msgs"][0]["content"]
+                    print("\nOther: {}".format(latest_msg))
                 break
 
+            if not waiting_msg:
+                print("等待对方回复." , end='')
+                waiting_msg = True
+            else:
+                print("..", end='')
             time.sleep(1)
-            print("等待对方回复...")
             continue
 
         user_input = input("You 😊: ")
@@ -45,17 +93,11 @@ if __name__ == "__main__":
             break
         elif len(user_input.strip()) == 0:
             continue
-        # print(user_input)
 
-        rsp = requests.post(
-            "http://192.168.110.207:8000/sendChatMsg",
-            json={
-                "user_id": user_id,
-                "msg":  user_input.strip(),
-                "room_id": room_id,
-            },
-        )
-        if rsp is None or rsp.status_code != 200:
-            print("error: {}".format(rsp.text))
-            exit(0)
+        # 发送消息
+        send_chat(user_id=user_id, room_id=room_id, user_input=user_input)
         pass
+
+
+if __name__ == "__main__":
+    main()
